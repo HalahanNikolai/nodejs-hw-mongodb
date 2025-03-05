7e780adfaef10523e3055873a458e81ae6c646cc
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2025-03-05 21:18:16 +0200

    Fix-1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-// import cors from 'cors';
 import initMongoConnection from './db/initMongoConnection.js';
 import setUpServer from './server.js';
 
@@ -8,26 +6,3 @@ export const bootstrap = async () => {
     setUpServer();
 };
 bootstrap();
-
-
-// setUpServer();
-
-// app.use(cors());
-
-// app.use((req, res, next) => {
-//     const { key } = req.query;
-//     console.log(key);
-//     if (key !== '12345678') {
-//         return res.status(401).send({ message: 'Please provide valid key' });
-//     }
-//     next();
-// });
-
-// app.get('/', (req, res) => {
-//     res.status(200);
-//     res.send('Get: Hello Nikolai!!!');
-// });
-
-// app.listen(8080, () => {
-//     console.log('Server started on port 8080');
-// });
